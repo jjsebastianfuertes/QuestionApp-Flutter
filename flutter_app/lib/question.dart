59dacad943704eec8ed4968e570d4cd68463fe0e
jjsebastianfuertes <jjsebastianfuertes@gmail.com>
2019-07-25 13:18:11 -0500

    create question widget

---
+++ flutter_app/lib/question.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  final String questionText;
+
+  Question(this.questionText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(vertical: 20.0),
+      child: Text(
+        questionText,
+        style: TextStyle(
+          fontSize: 25.0,
+          color: Colors.black45,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
